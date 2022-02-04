b57cbaf8f8f6f8576e3e4a827f4c9e9068c0f3e3
simone <simon387@hotmail.it>
2022-02-04 23:04:56 +0100

    api_util

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,4 +21,12 @@ Route::post('login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->group(function () {
 	Route::get('user', [AuthController::class, 'user']);
 	Route::post('logout', [AuthController::class, 'logout']);
+
+//	Route::get('users', [UserController::class], 'index');
+//	Route::post('users', [UserController::class], 'store');
+//	Route::get('users/{id}', [UserController::class], 'show');
+//	Route::put('users/{id}', [UserController::class], 'update');
+//	Route::delete('users/{id}', [UserController::class], 'destroy');
+	// i 5 metodi sopra possono essere anche scritti con un metodo solo:
+	Route::apiResource('users', UserController::class);
 });
