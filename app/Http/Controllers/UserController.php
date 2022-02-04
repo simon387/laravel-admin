53b30db6f605a7833eacec5ae1494706c106d550
simone <simon387@hotmail.it>
2022-02-04 23:37:51 +0100

    added_all_methods_4_user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,32 +2,46 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Symfony\Component\HttpFoundation\Response;
 
 class UserController extends Controller
 {
 	public function index()
 	{
-		//
+		return User::all();
 	}
 
 	public function store(Request $request)
 	{
-		//
+		$user = User::create(
+				$request->only('first_name', 'last_name', 'email')
+				+ ['password' => Hash::make(1234)]
+		);
+
+		return response($user, Response::HTTP_CREATED);
 	}
 
 	public function show($id)
 	{
-		//
+		return User::find($id);
 	}
 
 	public function update(Request $request, $id)
 	{
-		//
+		$user = User::find($id);
+
+		$user->update($request->only('first_name', 'last_name', 'email'));
+
+		return \response($user, Response::HTTP_ACCEPTED);
 	}
 
 	public function destroy($id)
 	{
-		//
+		User::destroy($id);
+
+		return \response(null, Response::HTTP_NO_CONTENT);
 	}
 }
