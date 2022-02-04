164a70cc68992f7e25bdeedfdd97d09b0dfcd1d5
simone <simon387@hotmail.it>
2022-02-04 20:12:11 +0100

    login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\RegisterRequest;
 use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -19,5 +21,23 @@ class AuthController extends Controller
 		]);
 
 		return response($user, Response::HTTP_CREATED);
+	}
+
+	public function login(Request $request)
+	{
+		if (!Auth::attempt($request->only('email', 'password'))) {
+			return \response([
+					'error' => 'Invalid credentials!'
+			], Response::HTTP_UNAUTHORIZED);
+		}
+
+		/** @var User $user */
+		$user = Auth::user();
+
+		$token = $user->createToken('token')->plainTextToken;
+
+		return \response([
+				'jwt' => $token
+		]);
 	}
 }
