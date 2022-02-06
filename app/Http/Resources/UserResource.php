4f16167d6995583fb5fa636af0280e3105fcf543
simone celia <simone.celia@thinkopen.it>
2022-02-06 20:29:38 +0100

    when_loaded

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -24,7 +24,7 @@ class UserResource extends JsonResource
 			'first_name' => $this->first_name,
 			'last_name' => $this->last_name,
 			'email' => $this->email,
-			'role' => $this->role,
+			'role' => new RoleResource($this->whenLoaded('role')),
 		];
 	}
 }
