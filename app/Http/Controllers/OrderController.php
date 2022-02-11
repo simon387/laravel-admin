54c1155827826b82298c0bee8602125eba99aa13
simone <simon387@hotmail.it>
2022-02-11 01:35:11 +0100

    chart

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -50,4 +50,17 @@ class OrderController extends Controller
 
 		return \Response::stream($callback, 200, $headers);
 	}
+
+	public function chart()
+	{
+		//  select DATE_FORMAT(orders.created_at, '%Y-%m-%d') as date, SUM(order_items.price * order_items.quantity) as sum
+		//  FROM orders
+		//  JOIN order_items on orders.id = order_id
+		//  GROUP BY date;
+		return Order::query()
+			->join('order_items', 'orders.id', '=', 'order_items.order_id')
+			->selectRaw("DATE_FORMAT(orders.created_at, '%Y-%m-%d') as date, SUM(order_items.price * order_items.quantity) as sum")
+			->groupBy('date')
+			->get();
+	}
 }
