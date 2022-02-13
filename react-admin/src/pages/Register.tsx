92e00cb4b2f700e3cecabe9473e42f986a6aba86
simone celia <simone.celia@thinkopen.it>
2022-02-13 22:20:49 +0100

    state

--- react-admin/src/pages/Register.tsx
+++ react-admin/src/pages/Register.tsx
@@ -1,6 +1,7 @@
 import React, {Component, SyntheticEvent} from "react";
 import "../Login.css";
 import axios from 'axios';
+import {Navigate} from "react-router-dom";
 
 class Register extends Component {
 	first_name = '';
@@ -8,11 +9,15 @@ class Register extends Component {
 	email = '';
 	password = '';
 	password_confirm = '';
+	state = {
+		redirect: false,
+	};
 
+	// @ts-ignore
 	submit = async (e: SyntheticEvent) => {
 		e.preventDefault();
 
-		const response = axios.post('http://localhost:8000/api/register', {
+		await axios.post('http://localhost:8000/api/register', {
 			first_name: this.first_name,
 			last_name: this.last_name,
 			email: this.email,
@@ -20,33 +25,39 @@ class Register extends Component {
 			password_confirm: this.password_confirm,
 		});
 
-		console.log(response);
+		this.setState({
+			redirect: true
+		});
 	}
 
 	render() {
-		return (
-			<main className="form-signin">
-				<form onSubmit={this.submit}>
-					<h1 className="h3 mb-3 fw-normal">Please register</h1>
-					<input className="form-control" placeholder="First Name" required
-						   onChange={e => this.first_name = e.target.value}
-					/>
-					<input className="form-control" placeholder="Last Name" required
-						   onChange={e => this.last_name = e.target.value}
-					/>
-					<input type="email" className="form-control" placeholder="Email" required
-						   onChange={e => this.email = e.target.value}
-					/>
-					<input type="password" className="form-control" placeholder="Password"
-						   onChange={e => this.password = e.target.value}
-					/>
-					<input type="password" className="form-control" placeholder="Password Confirm"
-						   onChange={e => this.password_confirm = e.target.value}
-					/>
-					<button className="w-100 btn btn-lg btn-primary" type="submit">Submit</button>
-				</form>
-			</main>
-		);
+		if (this.state.redirect) {
+			return <Navigate to={'/login'}/>
+		} else {
+			return (
+				<main className="form-signin">
+					<form onSubmit={this.submit}>
+						<h1 className="h3 mb-3 fw-normal">Please register</h1>
+						<input className="form-control" placeholder="First Name" required
+							   onChange={e => this.first_name = e.target.value}
+						/>
+						<input className="form-control" placeholder="Last Name" required
+							   onChange={e => this.last_name = e.target.value}
+						/>
+						<input type="email" className="form-control" placeholder="Email" required
+							   onChange={e => this.email = e.target.value}
+						/>
+						<input type="password" className="form-control" placeholder="Password"
+							   onChange={e => this.password = e.target.value}
+						/>
+						<input type="password" className="form-control" placeholder="Password Confirm"
+							   onChange={e => this.password_confirm = e.target.value}
+						/>
+						<button className="w-100 btn btn-lg btn-primary" type="submit">Submit</button>
+					</form>
+				</main>
+			);
+		}
 	}
 }
 
