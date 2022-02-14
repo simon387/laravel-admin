db3238f52d277ee373cbcdd0a752be333b021a90
simone <simon387@hotmail.it>
2022-02-14 18:51:13 +0100

    navlink

--- react-admin/src/components/Menu.tsx
+++ react-admin/src/components/Menu.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {Link} from 'react-router-dom';
+import {NavLink} from 'react-router-dom';
 
 //hooks method of component
 const Menu = () => {
@@ -8,14 +8,14 @@ const Menu = () => {
 			<div className="position-sticky pt-3">
 				<ul className="nav flex-column">
 					<li className="nav-item">
-						<Link to={'/'} className="nav-link active">
+						<NavLink to={'/'} className="nav-link">
 							Dashboard
-						</Link>
+						</NavLink>
 					</li>
 					<li className="nav-item">
-						<Link to={'/users'} className="nav-link active">
+						<NavLink to={'/users'} className="nav-link">
 							Users
-						</Link>
+						</NavLink>
 					</li>
 				</ul>
 			</div>
