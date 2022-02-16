89d934e6ec166539bd680df8ee01a65647d32c7c
simone <simon387@hotmail.it>
2022-02-16 01:53:24 +0100

    create_user_react

--- react-admin/src/App.tsx
+++ react-admin/src/App.tsx
@@ -5,15 +5,17 @@ import Dashboard from "./pages/Dashboard";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Users from "./pages/users/Users";
+import UserCreate from "./pages/users/UserCreate";
 
 function App() {
 	return (
 		<div className="App">
 			<BrowserRouter>
 				<Route path={'/'} exact component={Dashboard}/>
-				<Route path={'/users'} exact component={Users}/>
 				<Route path={'/register'} component={Register}/>
 				<Route path={'/login'} component={Login}/>
+				<Route path={'/users'} exact component={Users}/>
+				<Route path={'/users/create'} component={UserCreate}/>
 			</BrowserRouter>
 		</div>
 	);
