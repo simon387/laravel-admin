2b08348a0da13fe38e972369a56308cf3d6c3700
simone <simon387@hotmail.it>
2022-02-20 22:14:58 +0100

    fine_redux_ex

--- react-admin/src/components/Nav.tsx
+++ react-admin/src/components/Nav.tsx
@@ -2,22 +2,25 @@ import React, {useEffect, useState} from "react";
 import axios from "axios";
 import {Link} from "react-router-dom";
 import {User} from "../models/User";
+import {connect} from "react-redux";
 
-const Nav = () => {
-	const [user, setUser] = useState(new User());
+const Nav = (props: {user: User}) => {
+	// const [user, setUser] = useState(new User());
 
-	useEffect(() => {
-		(async () => {
-			const {data} = await axios.get('user');
-			setUser(new User(
-				data.id,
-				data.first_name,
-				data.last_name,
-				data.email,
-				data.role,
-			));
-		})()
-	}, []);
+	// useEffect(() => {
+	// 	(async () => {
+	// 		const {data} = await axios.get('user');
+	// 		setUser(new User(
+	// 			data.id,
+	// 			data.first_name,
+	// 			data.last_name,
+	// 			data.email,
+	// 			data.role,
+	// 		));
+	// 	})()
+	// }, []);
+	// VECCHIO METODO, SENZA REDUX
+
 
 	const logout = async () => {
 		await axios.post('logout', {});
@@ -27,7 +30,7 @@ const Nav = () => {
 		<nav className="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
 			<a className="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="#">Company name</a>
 			<ul className="my-2 my-md-0 mr-md-3">
-				<Link to="/profile" className="p-2 text-white text-decoration-none">{user.name}</Link>
+				<Link to="/profile" className="p-2 text-white text-decoration-none">{props.user.name}</Link>
 				<Link to="/login" className="p-2 text-white text-decoration-none"
 				   onClick={logout}>Sign out</Link>
 			</ul>
@@ -35,4 +38,11 @@ const Nav = () => {
 	);
 }
 
-export default Nav;
+const mapStateToProps = (state: { user: User }) => {
+	// is where we get in every component we will get the event from other components
+	return {
+		user: state.user, // here we get the user from the state
+	}
+}
+
+export default connect(mapStateToProps)(Nav);
--- react-admin/src/components/Wrapper.tsx
+++ react-admin/src/components/Wrapper.tsx
@@ -1,8 +1,11 @@
-import React, {useEffect, useState} from 'react';
+import React, {Dispatch, useEffect, useState} from 'react';
 import Nav from "./Nav";
 import Menu from "./Menu";
 import axios from "axios";
 import {Redirect} from "react-router-dom";
+import {connect} from 'react-redux';
+import {User} from "../models/User";
+import {setUser} from "../redux/actions/setUserAction";
 
 const Wrapper = (props: any) => {
 	const [redirect, setRedirect] = useState(false);
@@ -11,6 +14,16 @@ const Wrapper = (props: any) => {
 		(async () => {
 			try {
 				const {data} = await axios.get('user');
+
+				// redux
+				props.setUser(new User(
+					data.id,
+					data.first_name,
+					data.last_name,
+					data.email,
+					data.role,
+				));
+
 			} catch (e) {
 				setRedirect(true);
 			}
@@ -37,4 +50,18 @@ const Wrapper = (props: any) => {
 	);
 }
 
-export default Wrapper;
+const mapStateToProps = (state: { user: User }) => {
+	// is where we get in every component we will get the event from other components
+	return {
+		user: state.user, // here we get the user from the state
+	}
+}
+
+const mapDispatchToProps = (dispatch: Dispatch<any>) => {
+	// send the event to other components
+	return {
+		setUser: (user: User) => dispatch(setUser(user)) // here we dispatch the user using the action
+	}
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Wrapper);
