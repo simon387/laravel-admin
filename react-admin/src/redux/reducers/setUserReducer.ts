e83373264868fc8d6e605bd4874e1d4c640a60b8
simone <simon387@hotmail.it>
2022-02-20 21:58:31 +0100

    setupRedux

---
+++ react-admin/src/redux/reducers/setUserReducer.ts
@@ -0,0 +1,14 @@
+import {User} from "../../models/User";
+
+export const setUserReducer = (state = {user: new User()},
+							   action: { type: string, user: User }) => {
+	switch (action.type) {
+		case "SET_USER":
+			return {
+				...state,
+				user: action.user,
+			}
+		default:
+			return state;
+	}
+}
